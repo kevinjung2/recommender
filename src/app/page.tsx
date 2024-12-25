ab2765ec46fb7925fcb6cd28992487e2dc5e0384
Kevin Jung <kev.jung.ii@gmail.com>
2024-12-24 22:59:00 -0500

    sets up database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main>
+      {posts.map((post, ind) => (
+        <div key={ind}>{post.name}</div>
+      ))}
       <div className="flex items-center justify-center">Recommender</div>
     </main>
   );
