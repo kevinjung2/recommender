1fe554ece4558d874018b5f663e5fd098affb059
Kevin Jung <kev.jung.ii@gmail.com>
2024-12-25 01:19:29 -0500

    update table schema to expected final result

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,29 +8,112 @@ import {
   pgTableCreator,
   timestamp,
   varchar,
+  smallint,
+  uniqueIndex,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
+export const rolesEnum = pgEnum("roles", ["default", "admin"]);
+
 export const createTable = pgTableCreator((name) => `recommender_${name}`);
 
-export const posts = createTable(
-  "post",
+export const primaryCategories = createTable(
+  "primaryCategories",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (table) => ({
+    primaryNameIndex: uniqueIndex("primary_name_idx").on(table.name),
+  }),
+);
+
+export const secondaryCategories = createTable(
+  "secondaryCategories",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => ({
+    secondaryNameIndex: uniqueIndex("secondary_name_idx").on(table.name),
+  }),
+);
+
+export const users = createTable(
+  "users",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    firstName: varchar("first_name", { length: 256 }).notNull(),
+    lastName: varchar("last_name", { length: 256 }),
+    email: varchar("email", { length: 256 }).notNull(),
+    role: rolesEnum("roles").default("default"),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => {
+    return {
+      emailIndex: uniqueIndex("email_idx").on(table.email),
+    };
+  },
+);
+
+export const recommendations = createTable(
+  "recommendations",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }).notNull(),
+    primaryCategoryId: integer("primary_category_id").references(
+      () => primaryCategories.id,
+    ),
+    secondaryCategory: integer("secondary_category_id").references(
+      () => secondaryCategories.id,
+    ),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => ({
+    recommendationsIndex: index("recommendations_idx").on(table.name),
+  }),
+);
+
+export const reviews = createTable(
+  "reviews",
+  {
+    id: smallint("id").primaryKey().generatedByDefaultAsIdentity(),
+    rating: integer("rating").notNull(),
+    comment: varchar("comment", { length: 256 }).notNull(),
+    userId: integer("user_id").references(() => users.id),
+    recommendationId: integer("recommendation_id").references(
+      () => recommendations.id,
+    ),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => ({
+    ratingIndex: index("rating_idx").on(table.rating),
+  }),
 );
